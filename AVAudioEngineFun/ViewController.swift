845d88f402717e652191325e16bc44c396325c9d
John Forester <forester.john@gmail.com>
2014-06-05 14:40:56 -0500

    initial commit

--- AVAudioEngineFun/ViewController.swift
+++ AVAudioEngineFun/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-                            
+    
+    //MARK: - properties
+    let audioEngine: AVAudioEngine = AVAudioEngine()
+    let audioFilePlayer: AVAudioPlayerNode = AVAudioPlayerNode()
+    
+    @IBOutlet var filePlayerButton : UIButton
+    @IBOutlet var filePlaySlider : UISlider
+    
+    //MARK: - UIViewController methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //UI setup
+        self.filePlaySlider.value = self.audioFilePlayer.volume
+        
+        //audio file
+        let filePath: String = NSBundle.mainBundle().pathForResource("developers", ofType: "mp3")
+        
+        println("\(filePath)")
+        
+        let fileURL: NSURL = NSURL.URLWithString(filePath)
+        
+        var error: NSError?
+        let file: AVAudioFile? = AVAudioFile(forReading: fileURL, error: &error)
+        
+        if let errorValue = error {
+            println("ERROR: \(errorValue.localizedDescription)")
+        }
+        
+        //player setup
+        let mixer: AVAudioMixerNode = self.audioEngine.mainMixerNode
+        
+        self.audioEngine.attachNode(self.audioFilePlayer)
+        self.audioEngine.connect(self.audioFilePlayer, to: mixer, format: file?.processingFormat)
+        
+        //start engine
+        var engineError: NSError?
+        self.audioEngine.startAndReturnError(&engineError)
+        self.audioFilePlayer.scheduleFile(file, atTime: nil, completionHandler:
+            {
+                println("done playing file")
+            })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func filePlayerSliderChanged(sender : AnyObject) {
+        self.audioFilePlayer.volume = self.filePlaySlider.value
+    }
+    
+    //MARK: - IBAction methods
+    @IBAction func playFileButtonPressed(sender : AnyObject) {
+        if (self.audioFilePlayer.playing) {
+            self.audioFilePlayer.pause()
+            self.filePlayerButton.setTitle("play file", forState: .Normal)
+        } else {
+            self.audioFilePlayer.play()
+            self.filePlayerButton.setTitle("pause", forState: .Normal)
+        }
+    }
 }
 
